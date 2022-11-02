93212d157cf39003582d781b2b51c596bdf25fb9
Minh Tran <minhtran.2404@outlook.com>
2022-11-02 18:26:23 +0700

    Update ReactJS Assignment

--- ReactJS_Assignment/Client/src/App.jsx
+++ ReactJS_Assignment/Client/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Container, Flex, VStack } from '@chakra-ui/react';
+import { Fragment } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Fragment>
+      <Flex minW='max-content' alignItems='center' gap='2' w="full" bg="black">
+      <Heading size='md'>Chakra App</Heading>
+      </Flex>
+      <Container maxW="container.xl" p={0}>
+        <Flex h="100vh" py={20}>
+          <VStack
+            w="full"
+            h="full"
+            p={10}
+            spacing={10}
+            alignItems="flex-start">
+
+          </VStack>
+          <VStack
+            w="full"
+            h="full"
+            p={10}
+            spacing={10}
+            alignItems="flex-start"
+            bg="gray.50">
+
+          </VStack>
+        </Flex>
+      </Container>
+    </Fragment>
+
   )
 }
 
-export default App
+export default App;
